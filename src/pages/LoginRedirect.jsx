bfb54f7f5d53c14eb50d0a64a7492b991131cf6b
v15hv4 <vishva2912@gmail.com>
2020-06-04 04:31:11 +0530

    frontend: Store usergroup on token authentication

--- src/pages/LoginRedirect.jsx
+++ src/pages/LoginRedirect.jsx
@@ -6,8 +6,10 @@ class LoginRedirect extends Component {
    componentDidMount() {
       const token = this.props.location.search.substring(7);
       const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
+      const usergroup = this.props.location.search.split("&")[1].substring(10);
       localStorage.setItem("token", token);
       localStorage.setItem("expirationDate", expirationDate);
+      localStorage.setItem("usergroup", usergroup);
    }
 
    render() {
