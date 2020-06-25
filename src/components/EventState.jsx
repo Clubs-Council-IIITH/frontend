fcff7239b8aa462ff7ce6e5d210db3a646f37cf8
v15hv4 <vishva2912@gmail.com>
2020-06-26 02:02:59 +0530

    frontend: Highlight event state component

--- src/components/EventState.jsx
+++ src/components/EventState.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const State = (props) => {
    return (
       <React.Fragment>
-         <div className={"event-state state-" + props.current}> {props.current.toUpperCase()} </div>
+         <div className={"event-state my-3 px-2 state-" + props.current}>
+            {props.current.toUpperCase()}
+         </div>
       </React.Fragment>
    );
 };
