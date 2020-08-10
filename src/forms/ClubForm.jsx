b31d73d890487c002e7b3b41dc18f983b7c58cf6
v15hv4 <vishva2912@gmail.com>
2020-08-11 01:38:00 +0530

    frontend: Fix club name validation regex

--- src/forms/ClubForm.jsx
+++ src/forms/ClubForm.jsx
@@ -125,7 +125,7 @@ const ClubForm = (props) => {
                     invalid={errors.name}
                     type="text"
                     name="name"
-                    innerRef={register({ required: true, pattern: /^[a-zA-Z,. ]*$/ })}
+                    innerRef={register({ required: true, pattern: /^[a-zA-Z,.' ]*$/ })}
                 />
                 <FormFeedback> Club name can not be empty! </FormFeedback>
             </FormGroup>
