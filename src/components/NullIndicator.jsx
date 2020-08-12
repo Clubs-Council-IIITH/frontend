3bc78565dffcf054e588307d1ac67ef81a152be1
v15hv4 <vishva2912@gmail.com>
2020-08-13 02:09:40 +0530

    frontend: Use PageContainer for null indicators

--- src/components/NullIndicator.jsx
+++ src/components/NullIndicator.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Col } from "reactstrap";
+import Page from "./PageContainer";
 
 const NullIndicator = (props) => {
     return (
         <Col className="loading-container p-0 mx-0 my-4 my-md-5">
-            <img src="/shrug-18.svg" alt="empty" className="null-img" />
-            <div className="null-msg mt-3">{props.msg ? props.msg : "there's nothing here..."}</div>
+            <Page fluid className="loading-container">
+                <img src="/shrug-18.svg" alt="empty" className="null-img" />
+                <div className="null-msg mt-3">
+                    {props.msg ? props.msg : "there's nothing here..."}
+                </div>
+            </Page>
         </Col>
     );
 };
