6e9279a8632d7a7bbfb9c61f1a3955faf3711916
v15hv4 <vishva2912@gmail.com>
2020-08-15 13:30:32 +0530

    frontend: Hide draft link from previous proposals

--- src/components/items/ProposalItem.jsx
+++ src/components/items/ProposalItem.jsx
@@ -10,23 +10,29 @@ const ProposalItem = (props) => {
             <CardBody>
                 <div className="proposal-date"> {formatDateTime(props.datetime).date} </div>
                 <div className="proposal-time"> {formatDateTime(props.datetime).time} </div>
-                <div className="proposal-link mt-3">
-                    <InputGroup>
-                        <Input type="text" value={props.link} readonly />
-                        <InputGroupAddon addonType="append" className="proposal-link-btn">
-                            <Button
-                                color="secondary"
-                                className="open-btn"
-                                onClick={() => window.open(props.link, "_blank")}
-                            >
-                                <img src="/open-18.svg" className="btn-icon mb-1 mr-1" alt="O" />
-                                <span> OPEN </span>
-                            </Button>
-                        </InputGroupAddon>
-                    </InputGroup>
-                </div>
+                {/* <div className="proposal-link mt-3"> */}
+                {/*     <InputGroup> */}
+                {/*         <Input type="text" value={props.link} readonly /> */}
+                {/*         <InputGroupAddon addonType="append" className="proposal-link-btn"> */}
+                {/*             <Button */}
+                {/*                 color="secondary" */}
+                {/*                 className="open-btn" */}
+                {/*                 onClick={() => window.open(props.link, "_blank")} */}
+                {/*             > */}
+                {/*                 <img src="/open-18.svg" className="btn-icon mb-1 mr-1" alt="O" /> */}
+                {/*                 <span> OPEN </span> */}
+                {/*             </Button> */}
+                {/*         </InputGroupAddon> */}
+                {/*     </InputGroup> */}
+                {/* </div> */}
                 <div className="proposal-pdf mt-3">
-                    <Button tag={Link} target="_blank" to={props.pdf} className="open-btn w-100">
+                    <Button
+                        size="lg"
+                        tag={Link}
+                        target="_blank"
+                        to={props.pdf}
+                        className="open-btn w-100"
+                    >
                         <img src="/view-18.svg" className="btn-icon mb-1 mr-1" alt="V" />
                         <span> VIEW PDF </span>
                     </Button>
