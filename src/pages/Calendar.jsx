60ce1cbeb2bc31cded17e5a842b47bdf44b70401
v15hv4 <vishva2912@gmail.com>
2020-08-16 17:01:47 +0530

    frontend: Add proper CC Events Calendar URI

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 import Page from "../components/PageContainer";
 
-const CALENDAR_URI = "";
+const CALENDAR_URI =
+    "https://outlook.office365.com/owa/calendar/46728502c54740bfb04bd9522fdccf17@iiit.ac.in/7d469f1382ef43cc84e65ac4d9e95d8710357110675142350263/calendar.html";
 
 const Calendar = () => {
     return (
