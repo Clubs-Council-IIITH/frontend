ca4f2c7f4edeaf4a20ce117222e65f99ec683f46
v15hv4 <vishva2912@gmail.com>
2020-08-20 16:15:39 +0530

    frontend: Add temporary placeholder for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+import Page from "../components/PageContainer";
+
 const Home = () => {
-    return <React.Fragment />;
+    return (
+        <Page fluid>
+            <div className="error-page mt-4">
+                <img src="/hello.svg" alt="hello" className="hello-banner" />
+                <div className="hello-msg mt-5">
+                    <div className="hello-header">hello there! </div>
+                    this page is still under construction. <br />
+                    why don't you check out the other pages meanwhile? <br />
+                </div>
+            </div>
+        </Page>
+    );
 };
 
 export default Home;
