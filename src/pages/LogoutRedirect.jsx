4fddbb3395303445c836340ac836b7812ab2698c
v15hv4 <vishva2912@gmail.com>
2020-08-24 03:56:25 +0530

    frontend: Fix logout redirect on proxied urls

--- src/pages/LogoutRedirect.jsx
+++ src/pages/LogoutRedirect.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react"; // eslint-disable-line
+import { Redirect } from "react-router-dom";
 
 import API from "../api/methods";
 
 import "../config";
 
 const LogoutRedirect = () => {
+    const url = global.config.logoutRedirectUrl;
+
     useEffect(() => {
         API.logout();
+        if (url[0] === "/") return <Redirect to={url} />;
         window.location.href = global.config.logoutRedirectUrl;
     });
     return null;
