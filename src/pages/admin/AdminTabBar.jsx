901661432ded4d887255f2b0176b8fb8c6533b6a
v15hv4 <vishva2912@gmail.com>
2020-09-18 21:31:57 +0530

    frontend: Add custom styles to collapsed tab bar

--- src/pages/admin/AdminTabBar.jsx
+++ src/pages/admin/AdminTabBar.jsx
@@ -13,27 +13,34 @@ const AdminTabBar = () => {
     ];
 
     return (
-        <>
-            <ul class="nav mb-2 d-none d-sm-block">
+        <div className="mb-3 mb-md-2">
+            <ul class="nav d-none d-sm-block">
                 <RoutedTabs startPathWith="/admin">
                     {tabs.map((tab) => (
                         <NavTab to={tab.to}>{tab.title}</NavTab>
                     ))}
                 </RoutedTabs>
             </ul>
-            <Button id="tabs" className="w-100 d-block d-sm-none">
+            <Button
+                id="tabs"
+                className="w-100 d-block d-sm-none text-uppercase common-btn nav-tab-collapse nav-tab-current dropdown-toggle"
+            >
                 {tabs.filter((tab) => tab.to === `/${location.pathname.split("/").pop()}`)[0].title}
             </Button>
-            <UncontrolledCollapse toggler="#tabs">
+            <UncontrolledCollapse toggler="#tabs" className="mb-2">
                 <RoutedTabs startPathWith="/admin">
                     {tabs.map((tab) => (
-                        <NavTab className="nav-tab-collapse" activeClassName="d-none" to={tab.to}>
+                        <NavTab
+                            className="nav-tab-collapse common-btn text-uppercase"
+                            activeClassName="d-none"
+                            to={tab.to}
+                        >
                             {tab.title}
                         </NavTab>
                     ))}
                 </RoutedTabs>
             </UncontrolledCollapse>
-        </>
+        </div>
     );
 };
 
