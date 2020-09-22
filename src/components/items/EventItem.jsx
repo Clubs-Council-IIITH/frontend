9bf3ac8668fc131fcff72931bc267191a5d7bfaa
v15hv4 <vishva2912@gmail.com>
2020-09-22 18:53:07 +0530

    frontend: Split event item into preview card and modal

--- src/components/items/EventItem.jsx
+++ src/components/items/EventItem.jsx
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { Alert, Button, Card, CardHeader, CardBody, CardFooter, Input } from "reactstrap";
+import React, { useState } from "react";
+import { Card, CardHeader, CardFooter } from "reactstrap";
 
 import EventState from "../EventState";
 import EditButton from "../buttons/EditButton";
 import DeleteButton from "../buttons/DeleteButton";
+import ViewEventModal from "../ViewEventModal";
 import EditEventModal from "../EditEventModal";
 import DeleteEventModal from "../DeleteEventModal";
-import { formatAudience } from "../../utils/AudienceFormatter";
 import { formatDateTime } from "../../utils/DateTimeFormatter";
 
 const EventItem = (props) => {
+    const [viewModal, setViewModal] = useState(false);
     const [editModal, setEditModal] = useState(false);
     const [deleteModal, setDeleteModal] = useState(false);
-    const [linkAlert, setLinkAlert] = useState(false);
-    const [hasLink, setHasLink] = useState(false);
+
+    const toggleViewModal = () => {
+        setViewModal(!viewModal);
+    };
 
     const toggleEditModal = () => {
         setEditModal(!editModal);
@@ -23,31 +26,9 @@ const EventItem = (props) => {
         setDeleteModal(!deleteModal);
     };
 
-    const toggleLinkAlert = () => {
-        setLinkAlert(!linkAlert);
-    };
-
-    const LinkButton = () => {
-        return (
-            <Button
-                color="info"
-                onClick={toggleLinkAlert}
-                className="link-btn mx-1"
-                active={linkAlert}
-            >
-                <img tag={Button} src="/link-18.svg" alt="L" className="link-icon" />
-            </Button>
-        );
-    };
-
-    useEffect(() => {
-        // eslint-disable-next-line no-useless-escape
-        const urlPattern = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim;
-        setHasLink(urlPattern.test(props.venue));
-    }, [props.venue]);
-
     return (
-        <Card className="event-card elevate">
+        <Card className="event-card elevate clickable">
+            <ViewEventModal modal={viewModal} toggleModal={toggleViewModal} instance={props} />
             <EditEventModal modal={editModal} toggleModal={toggleEditModal} id={props.id} />
             <DeleteEventModal
                 modal={deleteModal}
@@ -55,54 +36,29 @@ const EventItem = (props) => {
                 id={props.id}
                 name={props.name}
             />
-            <CardHeader>
-                <div className="event-datetime mb-2">{formatDateTime(props.datetime).datetime}</div>
+            <CardHeader className="mb-2" onClick={toggleViewModal}>
+                <div className="event-datetime my-2">{formatDateTime(props.datetime).datetime}</div>
                 <div className="event-name mb-2">{props.name}</div>
-                <EventState current={props.state} />
             </CardHeader>
-            <CardBody>
-                <div className="event-duration mb-2 text-lowercase">
-                    <span>
-                        <img className="card-icon" src="/clock-18.svg" alt="D" />
-                    </span>
-                    {props.duration}
-                </div>
-                {!hasLink ? (
-                    <div className="event-venue mb-2">
-                        <span>
-                            <img className="card-icon" src="/venue-18.svg" alt="V" />
-                        </span>
-                        {props.venue}
-                    </div>
-                ) : null}
-                <div className="event-audience mb-2">
-                    <span>
-                        <img className="card-icon" src="/audience-18.svg" alt="A" />
-                    </span>
-                    {formatAudience(props.audience)}
-                </div>
-            </CardBody>
-            {props.modifiable || hasLink ? (
-                <CardFooter className="text-right px-2">
-                    {hasLink ? <LinkButton /> : null}
-                    {props.modifiable ? (
-                        <>
+
+            <CardFooter className="d-flex flex-row justify-content-between">
+                {props.modifiable ? (
+                    <>
+                        <EventState current={props.state} />
+                        <div className="text-right mx-n2">
                             <EditButton onClick={toggleEditModal} />
                             <DeleteButton onClick={toggleDeleteModal} />
-                        </>
-                    ) : null}
-                </CardFooter>
-            ) : null}
-
-            <Alert
-                color="info"
-                isOpen={linkAlert}
-                toggle={toggleLinkAlert}
-                className="link-alert mx-2 px-2 my-2"
-            >
-                <div className="mb-2"> Meeting Link </div>
-                <Input id="linkfield" type="text" value={props.venue} readonly />
-            </Alert>
+                        </div>
+                    </>
+                ) : (
+                    <div
+                        className="view-event-indicator text-uppercase text-center w-100 font-weight-bold p-2 mb-2"
+                        onClick={toggleViewModal}
+                    >
+                        View
+                    </div>
+                )}
+            </CardFooter>
         </Card>
     );
 };
