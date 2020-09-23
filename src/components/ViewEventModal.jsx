b2825db7526fd5ce63aa5ad9088d818f77f4cbe9
v15hv4 <vishva2912@gmail.com>
2020-09-23 21:11:33 +0530

    frontend: Replace placeholder with actual event description in event display modal

--- src/components/ViewEventModal.jsx
+++ src/components/ViewEventModal.jsx
@@ -25,13 +25,7 @@ const ViewEventModal = (props) => {
                 <Row>
                     <Col lg="8" className="mb-2">
                         <div className="event-view-description my-2">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                            occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum.
+                            {props.instance.description}
                         </div>
                     </Col>
                     <Col className="ml-lg-2">
