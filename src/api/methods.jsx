ca560caae9a63d38ef03566605ce4e017b2e43fc
v15hv4 <vishva2912@gmail.com>
2020-11-06 21:27:33 +0530

    frontend: Document API methods and increase access token lifetime

--- src/api/methods.jsx
+++ src/api/methods.jsx
@@ -10,19 +10,25 @@ function fetchToken() {
 }
 
 export default {
+    // redirect to CAS and log the user in
     login: async (props) => {
         const token = props.location.search.split("&")[0].substring(7);
-        const expiration_date = new Date(new Date().getTime() + 3600 * 1000);
+
+        // expire the token after 1 week
+        const expiration_date = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
+
         localStorage.setItem("token", "Token " + token);
         localStorage.setItem("expiration_date", expiration_date);
     },
 
+    // clear session data from localStorage to log the user out
     logout: async () => {
         localStorage.removeItem("token");
         localStorage.removeItem("expiration_date");
         return await axios.get("/api/endsession");
     },
 
+    // get current session details from the server
     session: async () => {
         try {
             return await axios.get("/api/session/", { headers });
@@ -33,6 +39,7 @@ export default {
         }
     },
 
+    // db READ operation
     view: async (model, params) => {
         fetchToken();
         if (!headers.Authorization) return await axios.get("/api/" + model + "/", { params });
@@ -43,6 +50,7 @@ export default {
         }
     },
 
+    // db CREATE operation
     new: async (model, data) => {
         fetchToken();
         try {
@@ -52,6 +60,7 @@ export default {
         }
     },
 
+    // db UPDATE operation
     edit: async (model, id, data) => {
         fetchToken();
         try {
@@ -61,6 +70,7 @@ export default {
         }
     },
 
+    // db DELETE operation
     delete: async (model, id, data = {}) => {
         fetchToken();
         try {
