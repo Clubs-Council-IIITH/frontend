2012982e1dc00463cee887abd32dfd8b16638e17
v15hv4 <vishva2912@gmail.com>
2021-01-02 15:37:30 +0530

    rewrite: Add updates icon

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,7 +1,15 @@
 import "./styles.scss";
+import updatesIcon from "./assets/updates.svg";
 
 const Topbar = ({ title }) => {
-    return <div className="topbar">{title}</div>;
+    return (
+        <div className="topbar text-light">
+            <div className="title-display">{title}</div>
+            <div className="updates-display">
+                <img src={updatesIcon} alt="updates" className="updates-icon" />
+            </div>
+        </div>
+    );
 };
 
 export default Topbar;
