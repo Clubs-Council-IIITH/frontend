85d33be42a958c95e7950b6b3e599c3f1ba1aa4a
v15hv4 <vishva2912@gmail.com>
2021-01-07 18:49:01 +0530

    rewrite: Implement back button functionality

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -1,10 +1,14 @@
+import { useHistory } from "react-router-dom";
+
 import "./styles.scss";
 import backIcon from "./assets/back.svg";
 import { Button } from "reactstrap";
 
 const BackButton = () => {
+    const history = useHistory();
+
     return (
-        <Button color="light" className="back-btn rounded-circle">
+        <Button color="light" className="back-btn rounded-circle" onClick={() => history.goBack()}>
             <img src={backIcon} alt="back" className="back-icon" />
         </Button>
     );
