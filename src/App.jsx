2b0d58396563a1c1590e8c540397bbc017958e9e
v15hv4 <vishva2912@gmail.com>
2021-01-08 11:34:02 +0530

    rewrite: Implement auth and session

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
+import { HandleSession } from "api/session";
 
 import * as Public from "pages/public";
 
 const App = () => {
+    const [session, fetchSession, dispatch] = HandleSession();
+    useEffect(() => fetchSession(), []); // eslint-disable-line
+
+    console.log(session);
+
     return (
         <Switch>
+            {/* <div> */}
+            {/*     <Button onClick={() => dispatch({ type: "LOGIN" })}> login </Button> */}
+            {/*     <Button onClick={() => dispatch({ type: "LOGOUT" })}> logout </Button> */}
+            {/* </div> */}
             <Route exact path="/">
                 <Public.Home />
             </Route>
---
+++ src/api/session.jsx
@@ -0,0 +1,87 @@
+import { useReducer, useCallback } from "react";
+import axios from "axios";
+import "config";
+
+export const headers = () => ({
+    "Content-Type": "multipart/form-data",
+    Authorization: localStorage.getItem("token") || null,
+});
+
+export const initial = {
+    is_authenticated: false,
+    user: {
+        name: null,
+        group: null,
+    },
+    error: null,
+};
+
+export const HandleLogin = async () => {
+    const search = window.location.search;
+    const token = search.split("&")[0].substring(7);
+
+    if (token) {
+        // expire the token after 1 week
+        const expiration_date = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
+
+        localStorage.setItem("token", "Token " + token);
+        localStorage.setItem("expiration_date", expiration_date);
+
+        window.location.href = "/";
+    }
+};
+
+export const HandleLogout = async () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expiration_date");
+
+    const res = await axios.get("/api/endsession");
+    if (res.status === 200) window.location.href = res.data.logout_url;
+};
+
+const reducer = (state = initial, { type, response } = {}) => {
+    switch (type) {
+        case "LOGIN":
+            window.location.replace(global.config.loginUrl);
+            return state;
+        case "AUTOLOGIN":
+            HandleLogin();
+            return {
+                ...state,
+                is_authenticated: response.is_authenticated,
+                user: response.user,
+            };
+        case "LOGOUT":
+            HandleLogout();
+            return {
+                ...state,
+                is_authenticated: false,
+                user: initial.user,
+            };
+        case "ERROR":
+            return {
+                ...state,
+                is_authenticated: false,
+                user: initial.user,
+                error: response,
+            };
+        default:
+            return state;
+    }
+};
+
+export const HandleSession = () => {
+    const [state, dispatch] = useReducer(reducer, initial);
+
+    const makeRequest = useCallback(async () => {
+        try {
+            const response = await axios.get("/api/session", { headers: headers() });
+            dispatch({ type: "AUTOLOGIN", response: response.data });
+        } catch (err) {
+            const error = err.response;
+            dispatch({ type: "ERROR", response: error.data });
+        }
+    }, []);
+
+    return [state, makeRequest, dispatch];
+};
