00b912e5342674d5fbf44f8b23e02228cc37d7f2
v15hv4 <vishva2912@gmail.com>
2021-07-09 23:18:37 +0530

    Introduce MainContainer

---
+++ src/components/MainContainer/MainContainer.jsx
@@ -0,0 +1,58 @@
+import clsx from "clsx";
+
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { AppBar, Toolbar, CssBaseline } from "@material-ui/core";
+
+import Navigation from "components/Navigation";
+
+// styles {{{
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+    },
+    toolbar: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        padding: theme.spacing(0, 2),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(2),
+    },
+}));
+// }}}
+
+const MainContainer = ({ navigation, children }) => {
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <Navigation navigation={navigation} open={open} setOpen={setOpen} />
+
+            <AppBar
+                color="white"
+                elevation={0}
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar />
+            </AppBar>
+
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default MainContainer;
