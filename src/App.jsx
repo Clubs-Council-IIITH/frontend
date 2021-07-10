711ac62ac44a3bd507f56110d55d42255eb09d52
v15hv4 <vishva2912@gmail.com>
2021-07-10 19:04:41 +0530

    Fix dynamic route loading delay

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useEffect, useContext } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-// theme overrides
+// theme overrides {{{
 import { Theme } from "theme";
 import { MuiThemeProvider } from "@material-ui/core/styles";
+// }}}
 
-// constants
+// constants {{{
 import { userRoles } from "constants/userRoles";
+// }}}
 
-// contexts
+// contexts {{{
 import { NavigationContext } from "contexts/NavigationContext";
 import { SessionContext } from "contexts/SessionContext";
+// }}}
 
-// components
+// components {{{
 import {
     HomeOutlined,
     ExploreOutlined,
@@ -21,11 +24,60 @@ import {
     GroupOutlined,
 } from "@material-ui/icons";
 import MainContainer from "components/MainContainer";
+// }}}
 
-// pages
+// pages {{{
 import * as Public from "pages/Public";
 import * as Admin from "pages/Admin";
 import * as Club from "pages/Club";
+// }}}
+
+// routes {{{
+const publicRoutes = [
+    {
+        title: "Home",
+        path: "/",
+        icon: HomeOutlined,
+        component: <Public.Home />,
+    },
+    {
+        title: "Clubs",
+        path: "/clubs",
+        icon: ExploreOutlined,
+        component: <Public.Clubs />,
+    },
+    {
+        title: "Calendar",
+        path: "/calendar",
+        icon: CalendarTodayOutlined,
+        component: <Public.Calendar />,
+    },
+];
+
+const adminRoutes = [
+    {
+        title: "Manage Clubs",
+        path: "/admin/clubs",
+        icon: DashboardOutlined,
+        component: <Admin.Clubs />,
+    },
+    {
+        title: "Manage Users",
+        path: "/admin/users",
+        icon: GroupOutlined,
+        component: <Admin.Users />,
+    },
+];
+
+const clubRoutes = [
+    {
+        title: "Manage Club",
+        path: "/manage/club",
+        icon: DashboardOutlined,
+        component: <Club.About />,
+    },
+];
+// }}}
 
 const App = () => {
     const { session } = useContext(SessionContext);
@@ -33,52 +85,11 @@ const App = () => {
 
     // site navigation
     useEffect(() => {
-        const publicRoutes = [
-            {
-                title: "Home",
-                path: "/",
-                icon: HomeOutlined,
-                component: <Public.Home />,
-            },
-            {
-                title: "Clubs",
-                path: "/clubs",
-                icon: ExploreOutlined,
-                component: <Public.Clubs />,
-            },
-            {
-                title: "Calendar",
-                path: "/calendar",
-                icon: CalendarTodayOutlined,
-                component: <Public.Calendar />,
-            },
-        ];
-
         const protectedRoutes =
             session?.user?.group === userRoles.admin
-                ? [
-                      {
-                          title: "Manage Clubs",
-                          path: "/admin/clubs",
-                          icon: DashboardOutlined,
-                          component: <Admin.Clubs />,
-                      },
-                      {
-                          title: "Manage Users",
-                          path: "/admin/users",
-                          icon: GroupOutlined,
-                          component: <Admin.Users />,
-                      },
-                  ]
+                ? adminRoutes
                 : session?.user?.group === userRoles.coordinator
-                ? [
-                      {
-                          title: "Manage Club",
-                          path: "/manage/club",
-                          icon: DashboardOutlined,
-                          component: <Club.About />,
-                      },
-                  ]
+                ? clubRoutes
                 : [];
 
         setNavigation({
@@ -94,14 +105,14 @@ const App = () => {
                     {!!Object.values(navigation).flat().length && (
                         <Switch>
                             {/* public routes */}
-                            {Object.values(navigation.publicRoutes).map((route, idx) => (
+                            {publicRoutes.map((route, idx) => (
                                 <Route exact path={route.path} key={idx}>
                                     {route.component}
                                 </Route>
                             ))}
 
                             {/* protected routes */}
-                            {Object.values(navigation.protectedRoutes).map((route, idx) => (
+                            {[...adminRoutes, ...clubRoutes].map((route, idx) => (
                                 <Route exact path={route.path} key={idx}>
                                     {route.component}
                                 </Route>
