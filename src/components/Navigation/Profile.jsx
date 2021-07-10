4c6a14f07dd53bb16ec473e7400fe6da8dc29c24
v15hv4 <vishva2912@gmail.com>
2021-07-10 05:59:49 +0530

    Make profile avatar smaller

--- src/components/Navigation/Profile.jsx
+++ src/components/Navigation/Profile.jsx
@@ -14,7 +14,10 @@ const Profile = () => {
             {session?.is_authenticated ? (
                 <>
                     <Box my={2} display="flex" alignItems="center">
-                        <Avatar src="" style={{ backgroundColor: "#111" }} />
+                        <Avatar
+                            src=""
+                            style={{ backgroundColor: "#111", width: "1.8em", height: "1.8em" }}
+                        />
                         <Box mx={2}>
                             <Box fontSize="1.4em">{session.user.name.split("@")[0]}</Box>
                             <Box>@{session.user.name.split("@")[1]}</Box>
