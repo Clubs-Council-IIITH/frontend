5d197b1fa155b1018f0e2f770e226aed83a976ca
v15hv4 <vishva2912@gmail.com>
2022-02-21 20:19:08 +0530

    Fix collapsed navigation bar width

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -53,7 +53,7 @@ const useStyles = makeStyles((theme) => ({
         overflowX: "hidden",
         width: theme.spacing(7) + 1,
         [theme.breakpoints.up("sm")]: {
-            width: theme.spacing(11) + 1,
+            width: theme.spacing(11) - 1,
         },
     },
     toolbar: {
