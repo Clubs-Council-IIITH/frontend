8db6f716698833086af2849802ee58991ce5266f
Bhav Beri <bhavberi@gmail.com>
2022-08-13 16:42:24 +0530

    Added few Images for Homepage, updated Gallery

--- src/pages/Public/Home/Gallery.jsx
+++ src/pages/Public/Home/Gallery.jsx
@@ -2,55 +2,50 @@ import { useTheme } from "@mui/styles";
 
 import { Box, ImageList, ImageListItem } from "@mui/material";
 
+import Img1 from "assets/img/gallery/1.jpg";
+import Img2 from "assets/img/gallery/2.jpg";
+import Img3 from "assets/img/gallery/3.jpeg";
+import Img4 from "assets/img/gallery/4.jpeg";
+import Img5 from "assets/img/gallery/5.jpg";
+import Img6 from "assets/img/gallery/6.jpeg";
+import Img7 from "assets/img/gallery/7.jpg";
+import Img8 from "assets/img/gallery/8.jpg";
+
+
 const itemData = [
     {
-        img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
-        title: "Bed",
+        img: Img1,
+        title: "Music-Stage",
     },
     {
-        img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
-        title: "Books",
+        img: Img2,
+        title: "Big-Events",
     },
     {
-        img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
-        title: "Sink",
+        img: Img3,
+        title: "Music",
     },
     {
-        img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
-        title: "Kitchen",
+        img: Img4,
+        title: "Sanskrit-Talks",
     },
     {
-        img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
-        title: "Blinds",
+        img: Img5,
+        title: "Singing",
     },
     {
-        img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
-        title: "Chairs",
+        img: Img6,
+        title: "Astronomy",
     },
     {
-        img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
-        title: "Laptop",
+        img: Img7,
+        title: "Felicity-Gaming-2021",
     },
     {
-        img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
-        title: "Doors",
+        img: Img8,
+        title: "Drum-Set",
     },
-    {
-        img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
-        title: "Coffee",
-    },
-    {
-        img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
-        title: "Storage",
-    },
-    {
-        img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
-        title: "Candle",
-    },
-    {
-        img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
-        title: "Coffee table",
-    },
+    
 ];
 
 const Gallery = () => {
