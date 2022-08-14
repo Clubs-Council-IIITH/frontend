0401198c9d3fe0bb4db0bbc4e1e233f304e9a1fd
Bhav Beri <43399374+bhavberi@users.noreply.github.com>
2022-08-14 15:18:58 +0530

    Updated About in Homepage

--- src/pages/Public/Home/About.jsx
+++ src/pages/Public/Home/About.jsx
@@ -8,28 +8,46 @@ const About = () => {
     return (
         <Box px={5} py={8} backgroundColor={theme.palette.primary.main}>
             <Typography variant="h4" color="secondary" fontWeight={500} mb={4}>
-                Clubs @ IIITH
+                Clubs Council @ IIITH
             </Typography>
             <Typography variant="h6" color="secondary" fontWeight={400}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae
-                vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda
-                officia quibusdam deleniti eos cupiditate dolore doloribus! Lorem ipsum dolor sit
-                amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus
-                eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam
-                deleniti eos cupiditate dolore doloribus! Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione
-                temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos
-                cupiditate dolore doloribus!
+                <i>
+                    The Clubs Council is the largest Student Administrative Organization at IIIT Hyderabad,
+                    and acts as an umbrella body of all the institute affiliated and associate student-led
+                    Clubs, Groups & Societies.
+                </i>
             </Typography>
             <Typography variant="h6" color="secondary" mt={3} fontWeight={400}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae
-                vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda
-                officia quibusdam deleniti eos cupiditate dolore doloribus!
+                {'\u2B24'} 2 Secretaries
             </Typography>
             <Typography variant="h6" color="secondary" mt={3} fontWeight={400}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae
-                vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda
-                officia quibusdam deleniti eos cupiditate dolore doloribus!
+                {'\u2B24'} 5 Undersecretaries
+            </Typography>
+            <Typography variant="h6" color="secondary" mt={3} fontWeight={400}>
+                {'\u2B24'} 3 Tech Team Members
+            </Typography>
+            <Typography variant="h6" color="secondary" mt={3} fontWeight={400}>
+                {'\u2B24'} 23+ Student constituent groups, with more being planned.
+            </Typography>
+            <Typography variant="h6" color="secondary" mt={3} fontWeight={400}>
+                {'\u2B24'} 50+ Student Coordinators
+            </Typography>
+            <Typography variant="h6" color="secondary" mt={3} fontWeight={400}>
+                {'\u2B24'} 250+ Organizing team members
+            </Typography>
+            <Typography variant="h6" color="secondary" mt={3} fontWeight={400}>
+            </Typography>
+            <Typography variant="h4" color="secondary" fontWeight={500} mb={4}>
+                Clubs @ IIITH
+            </Typography>
+            <Typography variant="h6" color="secondary" mt={3} fontWeight={400}>
+                {'\u2192'} 7 Technical Clubs
+            </Typography>
+            <Typography variant="h6" color="secondary" mt={3} fontWeight={400}>
+                {'\u2192'} 16 Cultural Clubs
+            </Typography>
+            <Typography variant="h6" color="secondary" mt={3} fontWeight={400}>
+                {'\u2192'} More coming soon....
             </Typography>
         </Box>
     );
