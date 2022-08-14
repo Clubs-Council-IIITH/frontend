1e92f8d32f8dfeccba3b02ac4fb4913fd7dc491c
Bhav Beri <bhavberi@gmail.com>
2022-08-14 21:49:58 +0530

    Added More Pics to Carousal & Gallery

--- src/pages/Public/Home/Gallery.jsx
+++ src/pages/Public/Home/Gallery.jsx
@@ -12,6 +12,21 @@ import Img7 from "assets/img/gallery/7.jpg";
 import Img8 from "assets/img/gallery/8.jpg";
 import Img9 from "assets/img/gallery/9.jpg";
 import Img10 from "assets/img/gallery/10.jpg";
+import Img11 from "assets/img/gallery/11.jpg";
+import Img12 from "assets/img/gallery/12.jpeg";
+import Img13 from "assets/img/gallery/13.jpg";
+import Img14 from "assets/img/gallery/14.jpg";
+import Img15 from "assets/img/gallery/15.jpeg";
+
+import CarouselBackground1 from "assets/img/carousel/1.png";
+import CarouselBackground2 from "assets/img/carousel/2.jpg";
+import CarouselBackground3 from "assets/img/carousel/3.jpg";
+import CarouselBackground4 from "assets/img/carousel/4.jpeg";
+import CarouselBackground5 from "assets/img/carousel/5.jpg";
+import CarouselBackground6 from "assets/img/carousel/6.jpg";
+import CarouselBackground7 from "assets/img/carousel/7.jpg";
+import CarouselBackground8 from "assets/img/carousel/8.jpeg";
+import CarouselBackground9 from "assets/img/carousel/9.jpg";
 
 const itemData = [
     {
@@ -28,7 +43,7 @@ const itemData = [
     },
     {
         img: Img4,
-        title: "Sanskrit-Talks",
+        title: "Dance-Freshers",
     },
     {
         img: Img5,
@@ -53,6 +68,62 @@ const itemData = [
     {
         img: Img10,
         title: "Group-Pic-Debsoc",
+    },
+    {
+        img: Img11,
+        title: "Felicity-Dance",
+    },
+    {
+        img: Img12,
+        title: "Sanskrit-Talks",
+    },
+    {
+        img: Img13,
+        title: "KRB-Auditorium",
+    },
+    {
+        img: Img14,
+        title: "KRB",
+    },
+    {
+        img: Img15,
+        title: "Freshers-Dance",
+    },
+    {
+        img: CarouselBackground1,
+        title: "Into-to-Clubs",
+    },
+    {
+        img: CarouselBackground2,
+        title: "Roadblock",
+    },
+    {
+        img: CarouselBackground3,
+        title: "Debsoc-LokSabha",
+    },
+    {
+        img: CarouselBackground4,
+        title: "Fun-in-H105",
+    },
+    {
+        img: CarouselBackground5,
+        title: "Fail",
+    },
+    {
+        img: CarouselBackground6,
+        title: "Singing",
+    },
+    {
+        img: CarouselBackground7,
+        title: "Chess",
+    },
+    {
+        img: CarouselBackground8,
+        title: "Classical-Dance",
+    },
+    {
+        img: CarouselBackground9,
+        title: "Himalaya",
     },
 ];
 
