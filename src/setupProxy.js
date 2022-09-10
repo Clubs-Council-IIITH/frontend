15a5ddd6d358b04086114010e9ff43be495441de
ek234 <71319388+ek234@users.noreply.github.com>
2022-09-10 11:00:23 +0530

    Reverted target of createProxyMiddleware to localhost

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         ["/graphql", "/accounts", "/api", "/media/"],
         createProxyMiddleware({
-            target: "http://192.168.1.162:8000",
+            target: "http://localhost:8000",
             changeOrigin: true,
         })
     );
